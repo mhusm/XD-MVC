ef3bdd609602ac3c39d999efa972a0c4c42f1596
unknown <husmannm@DARROCH.d.ethz.ch>
2015-02-25 09:48:41 +0100

    intial commit of gallery example

---
+++ Examples/Gallery/public/js/viewer.js
@@ -0,0 +1,6 @@
+$(document).on("polymer-ready", function () {
+	var viewer = document.querySelector('image-element');
+    Object.observe(XDp2p.myPosition, function (changes) {
+        viewer.offset = XDp2p.myPosition.value;
+    });
+});
---
+++ Examples/Gallery/readme.md
@@ -0,0 +1,15 @@
+Cross-device Gallery
+====================
+
+
+Version
+-
+
+0.1
+
+
+Installation
+------------
+1. `npm install`
+2. `npm start`
+3. Go to http://hostname:8082/gallery2.html
