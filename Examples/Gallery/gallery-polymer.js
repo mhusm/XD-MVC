eaae9305a7ac51ffb7e1a6d1ff0cf799ab98672e
husmannm <maria.husmann@inf.ethz.ch>
2015-03-04 09:21:05 +0100

    moved some files to deprecated folders, renamed the basic example

--- Examples/Gallery/gallery-polymer.js
+++ Examples/Gallery/gallery-polymer.js
@@ -1,95 +1,70 @@
 var XDmvcServer = require('xd-mvc/xdmvcserver.js');
 var xdmvc = new XDmvcServer();
-console.log(xdmvc);
-xdmvc.start(9000, 9001);
-xdmvc.on("connected", function(id){
-   console.log(id);
-});
 
 var connect = require('connect'),
     http = require('http'),
 	bodyParser = require('body-parser'),
-	serveStatic = require('serve-static'); 
-  
+	serveStatic = require('serve-static');
+
 var url = require('url');
 
 var app = connect().use(bodyParser.json()).use(serveStatic(__dirname + '/public'));
-//var app = connect().use(bodyParser.urlencoded({ extended: false, type:'application/json'  })).use(serveStatic(__dirname + '/public'));
+app.use("/gallery", handleGallery);
 var server = http.createServer(app);
 var fs = require('fs');
 var basePath = "\public\\images";
-var transient = {};
-var persistent = {};
-
-// TODO extract transient and persistent stuff into framework
-
-
-function handleGallery(req, res) {
-    var query = url.parse(req.url, true).query;
-    console.log(req.body);
-	
-    if (query.album) {
-        setCurrentAlbum(query.album);
-    }
-    if (req.body.changed) {
-        persistent = req.body.changed;
-        console.log(persistent);
-		console.log("changed");
-    }
-    
-	res.writeHead(200, {'Content-Type': 'text/json' });
-	res.write(JSON.stringify({transient: transient, persistent: persistent}));
-	res.end('\n');
-}
+var albums = [];
 
 function createModel() {
-	var files = fs.readdirSync(basePath),
+    var files = fs.readdirSync(basePath),
         thumbs,
-        albums = [],
-        album,
-        currentAlbum = "";
-    
-    
+        album;
+
     files.forEach(function (file) {
-	    thumbs = fs.readdirSync(basePath + "\\" + file + "\\thumbs");
+        thumbs = fs.readdirSync(basePath + "\\" + file + "\\thumbs");
         album = {title: file, url: "images/" + file + "/thumbs/" + thumbs[0]};
         albums.push(album);
     });
-    if (files.length > 0) {
-        currentAlbum = albums[0].title;
+
+    albums.forEach(function(album){
+        var title = album.title;
+        var files = fs.readdirSync(basePath + "\\" + title + "\\thumbs");
+        var largePath = "images/" + title + "/large/";
+        var thumbsPath = "images/" + title + "/thumbs/";
+        // Read images from directory
+        album.thumbs = files.map(function (f) {
+            return thumbsPath + f;
+        });
+        album.large = files.map(function (f) {
+            return largePath + f;
+        });
+    });
+
+    console.log(albums);
+ }
+
+function handleGallery(req, res, next){
+    var parsedUrl = url.parse(req.url, true)
+    var query = parsedUrl.query;
+    var path = parsedUrl.pathname.split("/");
+    console.log(parsedUrl.pathname);
+    console.log(path);
+
+    if (path.length === 2 && path[1].length ===0){
+        res.writeHead(200, {'Content-Type': 'text/json' });
+        res.write(JSON.stringify(albums));
+        res.end('\n');
+        console.log("sending data");
     }
-     
-    createAlbumModel(currentAlbum);
-    persistent.albums = albums;
-    transient.currentAlbum = currentAlbum;
-    
-}
 
-function setCurrentAlbum(newAlbum) {
-    createAlbumModel(newAlbum);
- //   transient.currentAlbum = newAlbum;
-   
 }
-
-function createAlbumModel(title) {
-	var files = fs.readdirSync(basePath + "\\" + title + "\\thumbs");
-    var largePath = "images/" + title + "/large/";
-    var thumbsPath = "images/" + title + "/thumbs/";
-	// Read images from directory
-	persistent.thumbs = files.map(function (f) {
-		return thumbsPath + f;
-	});
-	persistent.large = files.map(function (f) {
-		return largePath + f;
-	});
-    transient.current = 0;
- 
-   
-}
-
-app.use("/gallery", handleGallery);
 
 createModel();
+xdmvc.start(9000, 9001);
+xdmvc.on("objectChanged", function(id){
+    console.log(id);
+});
+
 server.listen(8082);
 
 
