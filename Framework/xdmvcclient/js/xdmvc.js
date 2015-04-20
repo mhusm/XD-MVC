7c53362197fa78a17a7808f2ea938f9bddf9631d
husmannm <maria.husmann@inf.ethz.ch>
2015-04-20 12:29:28 +0200

    bug fix for changing device ID

--- Framework/xdmvcclient/js/xdmvc.js
+++ Framework/xdmvcclient/js/xdmvc.js
@@ -113,14 +113,20 @@ var XDmvc = {
 		XDmvc.server.connectToDevice(deviceId);
 	},
 
-    disconnect: function (peerId) {
+    disconnect: function disconnect(peerId) {
         var conn = XDmvc.getConnectedDevice(peerId);
         if (conn) {
-            conn.connection.close();
-            XDmvc.removeConnection(conn);
+            conn.disconnect();
         }
-
     },
+
+    disconnectAll: function disconnectAll() {
+        XDmvc.connectedDevices.forEach(function(device){
+            device.disconnect();
+        });
+    },
+
+
 
 
     getConnectedDevice: function (peerId) {
@@ -398,15 +404,16 @@ Initialisation and configuration
 
     changeDeviceId: function (newId){
         if (newId !== this.deviceId) {
-            this.deviceId = newId;
+            XDmvc.deviceId = newId;
             localStorage.deviceId = newId;
-            this.device.id = this.deviceId;
+            XDmvc.device.id = XDmvc.deviceId;
             // If connected, disconnect and reconnect
-            if (this.peer) {
-                this.peer.destroy();
-                this.cleanUpConnections();
-                this.peer = null;
-                this.connectToServer();
+            if (XDmvc.server) {
+                XDmvc.server.disconnect();
+                XDmvc.server = null;
+                XDmvc.disconnectAll();
+                XDmvc.connectToServer();
+                // TODO reconnect previous connections?
             }
         }
     },
@@ -569,6 +576,11 @@ XDmvcServer.prototype.handleConnection = function handleConnection (connection){
 
 };
 
+XDmvcServer.prototype.disconnect = function disconnect (){
+    this.peer.destroy();
+    this.peer = null;
+};
+
 /*
 Connected Devices
 -----------------
@@ -695,4 +707,10 @@ ConnectedDevice.prototype.send = function send (msgType, data){
     } else {
         console.warn("Can not send message to device. Not connected to " +this.id );
     }
+};
+
+ConnectedDevice.prototype.disconnect = function disconnect (){
+    this.connection.close();
+    XDmvc.removeConnection(this);
+
 };
