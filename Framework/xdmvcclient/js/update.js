480d0afb897add9f939366bf59e055d2156a767b
husmannm <maria.husmann@inf.ethz.ch>
2015-06-12 14:01:10 +0200

    devices and roles working with Polymer 1.0

---
+++ Framework/xdmvcclient/js/update.js
@@ -0,0 +1,37 @@
+function synchronizeObject(path, original, owner){
+    var observer = new ObjectObserver(original);
+    var key;
+    observer.open(function(added, removed, changed){
+        // Deleted properties
+        for (key in removed) {
+            owner.set(path+"." +key, undefined);
+        }
+        // New and changed properties
+        for (key in changed) {
+            owner.set(path+"." +key, changed[key]);
+        }
+        for (key in added) {
+            owner.set(path+"." +key, added[key]);
+        }
+    });
+
+}
+
+function synchronizeArray(path, original, owner){
+    var observer = new ArrayObserver(original);
+    observer.open(function(splices){
+        splices.forEach(function(splice) {
+            var spliceArgs = [splice.index, splice.removed.length];
+            var addIndex = splice.index;
+            while (addIndex < splice.index + splice.addedCount) {
+                spliceArgs.push(original[addIndex]);
+                addIndex++;
+            }
+
+            var args = [path].concat(spliceArgs);
+            owner.splice.apply(owner, args);
+        });
+    });
+
+}
+
