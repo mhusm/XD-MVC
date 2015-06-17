6dcd7eeabe5749c8485e5c874986e3976e0811dc
Silvan Egli <siegli@student.ethz.ch>
2015-06-17 14:37:13 +0200

    strange git

--- Examples/Performance/public/js/performance_test.js
+++ Examples/Performance/public/js/performance_test.js
@@ -19,7 +19,7 @@ function initialize() {
         window.history.pushState('', '', window.location.search.length > 0? window.location.search +"&" +archString : '?'+archString);
         XDmvc.setPeerToPeer();
     } else{
-        if (arch ===  'p2p' || arch ===  XDmvc.peerToPeer) {
+        if (arch ===  'p2p' || arch ===  XDmvc.peerToPeer) { 
             XDmvc.setPeerToPeer();
             archString = 'architecture=' + XDmvc.peerToPeer;
             window.history.pushState('', '','?'+archString);
