55f7cbf2cdc5d0b62d6999dfbc81d9aa1ff3d15f
Silvan Egli <siegli@student.ethz.ch>
2015-07-01 11:45:02 +0200

    added js src

--- flightplan.js
+++ flightplan.js
@@ -17,15 +17,16 @@ plan.target('cloud', [
     }
 ]);
 
+
 // run commands on localhost
-plan.local(function(local) {
+plan.local(['all', 'performance'], function(local) {
     currBranch = local.exec('git rev-parse --abbrev-ref HEAD').stdout;
     local.log('Push files to origin ' + currBranch);
     local.exec('git push origin ' + currBranch);
 });
 
 // run commands on remote hosts (destinations)
-plan.remote(function(remote) {
+plan.remote('all',function(remote) {
     var appTopFolder = '~/code/xdmvc/Examples/'
     var appFolders = ['Gallery','Maps','Performance'];
 
@@ -43,6 +44,27 @@ plan.remote(function(remote) {
     remote.log('Reload  Maps application');
     remote.exec('forever stop ~/code/xdmvc/Examples/Maps/server.js', {failsafe: true});
     remote.exec('forever start ~/code/xdmvc/Examples/Maps/server.js');
+    remote.log('---------------------------------------------------------');
+    remote.log('Reload Performance application');
+    remote.exec('forever stop ~/code/xdmvc/Examples/Performance/perf_server.js', {failsafe: true});
+    remote.exec('forever start ~/code/xdmvc/Examples/Performance/perf_server.js');
+
+});
+
+plan.remote('performance',function(remote) {
+    var appTopFolder = '~/code/xdmvc/Examples/'
+    var appFolders = ['Performance'];
+
+    remote.log('pull from origin '+ currBranch);
+    remote.exec('cd /root/code/xdmvc && git checkout ' + currBranch + ' git pull origin ' + currBranch);
+
+
+    appFolders.forEach(function (appFolder) {
+        remote.log('-----------------------------------------------------------')
+        remote.log('running grunt for ' + appFolder);
+        remote.exec('cd '+appTopFolder + appFolder + ' && grunt && grunt server');
+    });
+
     remote.log('---------------------------------------------------------');
     remote.log('Reload Performance application');
     remote.exec('forever stop ~/code/xdmvc/Examples/Performance/perf_server.js', {failsafe: true});
