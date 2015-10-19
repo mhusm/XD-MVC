2fbe7b3780e60c5e230e44405d233df8368447b1
Silvan Egli <siegli@student.ethz.ch>
2015-10-19 23:30:23 +0200

    resolve port conflict for maps server

--- Examples/Maps/server.js
+++ Examples/Maps/server.js
@@ -7,7 +7,7 @@ var connect = require('connect'),
 var app = connect().use(serveStatic(__dirname + '/public'));
 var server = http.createServer(app);
 
-xdmvc.start(9000, 3000);
+xdmvc.start(9005, 3005, 9006);
 server.listen(8080);
 
 
